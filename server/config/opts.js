a762301d59b97ebe8620fb6ef2075deb0e49e03e
adrian jones <adrianj98@gmail.com>
2015-04-13 23:49:37 -0700

    change port

--- server/config/opts.js
+++ server/config/opts.js
@@ -3,5 +3,6 @@ module.exports = require('optimist')
     .usage('Usage: $0 --port [port]')
     .alias('port', 'p')
     .describe('port', 'Port number for the Express application.')
-    .default('port', 3000)
+    .default('port', process.env.PORT || 8080)
     .argv;
+ 
